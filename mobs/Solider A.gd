3c30ebaf6e18e210d7f9dca8a54d00d81ca7136f
BryceM7 <brycem417@gmail.com>
2024-09-16 14:08:46 -0400

    TD first day

---
+++ mobs/Solider A.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+
+@export var speed = 1000
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)

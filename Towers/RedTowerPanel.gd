6ff4cbadfabe407621090103e27d6b113698216b
BryceM7 <brycem417@gmail.com>
2024-10-02 14:08:31 -0400

    Debugged tower placement and added putting back towers you dont want

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -17,13 +17,20 @@ func _on_gui_input(event:InputEvent):
 		tempTower.scale = Vector2(0.32,0.32)
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#left click down drag
-		get_child(1).global_position = mousePosition
+		if get_child_count() > 1:
+			get_child(1).global_position = mousePosition
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#Left click up
-		print("Left button up")
-		get_child(1).queue_free()
-		var path = get_tree().get_root().get_node("Main/Towers")
+		if event.global_position.x >= -1362:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
 
-		path.add_child(tempTower)
-		tempTower.global_position = mousePosition
-		tempTower.get_node("Area").hide()
+		else:
+			var path = get_tree().get_root().get_node("Main/Towers")
+			path.add_child(tempTower)
+			tempTower.global_position = mousePosition
+			tempTower.get_node("Area").hide()
+
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
